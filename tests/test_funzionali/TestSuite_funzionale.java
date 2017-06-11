2cdc5b83f34211576b17d1d684774ac5e27dbfbc
Francesca <francesca.casale92@gmail.com>
2017-06-11 11:58:11 +0200

    Aggiunta cartella tests

---
+++ tests/test_funzionali/TestSuite_funzionale.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SQLDatabaseErrorTest.class, SQLDatabaseTest.class, UpdateSQLDatabaseErrorTest.class,
+		UpdateSQLDatabaseTest.class })
+public class TestSuite_funzionale {
+
+}
